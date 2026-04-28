4933dba0201ffc6fdd2dbdb0d2337f162563f4b0
Gadiza Fauzi <gadizafauzii@gmail.com>
2026-04-28 16:44:33 +0700

    chore: reorganize folder structure

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,21 @@
 
 namespace App\Providers;
 
-use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\MovieRepositoryInterface;
+use App\Repositories\MovieRepository;
+use Illuminate\Pagination\Paginator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        //
+        $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Paginator::useBootstrap();
     }
+
 }
